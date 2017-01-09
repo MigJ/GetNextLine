c0bf25fcd9c11727d66e1ab4c4b3744ed6259a03
master <miguel.joubert@epitech.eu>
2017-01-09 18:38:23 +0100

    x3 retry

--- get_next_line.c
+++ get_next_line.c
@@ -5,31 +5,10 @@
 ** Login   <miguel.joubert@epitech.eu>
 ** 
 ** Started on  Tue Jan  3 16:36:02 2017 Joubert Miguel
-** Last update Fri Jan  6 17:41:16 2017 Joubert Miguel
+** Last update Mon Jan  9 18:38:09 2017 Joubert Miguel
 */
 
 #include "get_next_line.h"
-
-int	my_ret_line(char *str, int i, int a)
-{
-  if (a == 0)
-    {
-      if (str[i] == 0)
-	return (848484);
-      while (str[i] != '\n')
-	{
-	  if (str[i] == 0)
-	    return (i);
-	  i++;
-	}
-    }
-  if (a == 1)
-    {
-      while (str[i] != 0)
-	i++;
-    }
-  return ((a == 0) ? i + 1 : i);
-}
 
 char	*my_strcat(char *dest, char *src)
 {
@@ -37,8 +16,7 @@ char	*my_strcat(char *dest, char *src)
   int   i;
   int   write;
 
-  res = malloc(sizeof(char) * my_ret_line(dest, 0, 1)
-	       + my_ret_line(src, 0, 1) + 1);
+  res = malloc(sizeof(char) * (strlen(dest) + strlen(src) + 1));
   i = -1;
   write = 0;
   while (dest[++i])
@@ -50,90 +28,48 @@ char	*my_strcat(char *dest, char *src)
   return (res);
 }
 
-char	*remalloc(char *str, int size, int a, int i)
+char	*get_next_line(const int fd)
 {
-  char	*tmp;
+  static char *str;
+  static char *buffer;
+  char *dest;
+  static int a;
+  static int k;
+  int	i;
+  int	tmp;
 
   if (a == 0)
     {
-      tmp = malloc(sizeof(char) * 1000);
-      if (str == 0)
-	return (tmp);
-      while (str[i] != 0)
+      k = 0;
+      buffer = malloc(sizeof(char) * READ_SIZE + 1);
+      str = malloc(sizeof(char) * READ_SIZE + 1);
+      *str = 0;
+      while ((i = read(fd, buffer, READ_SIZE)) > 0)
 	{
-	  tmp[i] = str[i];
-	  i++;
+	  tmp = i;
+	  buffer[tmp] = 0;
+	  str = my_strcat(str, buffer);
 	}
-      tmp[i] = 0;
+      a++;
     }
-  if (a == 1)
+  if (a != 0)
     {
-      while (str[i + READ_SIZE] != 0)
-	i++;
-      size += i;
-      while (i != size)
-	i++;
-      while (str[i] != 0)
-	str[i++] = 0;
-    }
-  return ((a == 0) ? tmp : str);
-}
-
-char	*get_next_line_fd_null(const int fd, int a, int k)
-{
-  t_StaticFd	S;
-  t_GetnextFd	G;
-
-  if (a == 0 && fd != 0)
-    {
-      G.dest = malloc(sizeof(char) * READ_SIZE);
-      S.str = malloc(sizeof(char) * READ_SIZE);
-      while ((k = read(fd, G.dest, READ_SIZE)) > 0)
+      i = 0;
+      dest = malloc(sizeof(char) * tmp);
+      while (str[k] != 0)
 	{
-	  G.tmp = k;
-	  G.dest[READ_SIZE] = 0;
-	  S.str = my_strcat(S.str, G.dest);
-	}
-      S.str = remalloc(S.str, G.tmp, 1, 0);
-    }
-  if (a != 0 && fd == 0)
-    {
-      while (read(fd, S.buffer, 1) > 0 && *S.buffer != '\n')
-	{
-	  G.dest = remalloc(G.dest, READ_SIZE, 0, 0);
-	  G.dest[k++] = *S.buffer;
-	  G.dest[k] = 0;
+	  if (str[k] == '\n')
+	    {
+	      dest[i] = 0;
+	      k++;
+	      return (dest);
+	    }
+	  dest[i] = str[k];
+	  i++, k++;
 	}
     }
-  return ((a == 0) ? S.str : G.dest);
-}
-
-char	*get_next_line(const int fd)
-{
-  t_Getnext	G;
-  static t_Static	S;
-
-  G.tmp = G.j = 0;
-  (S.a == 0 && fd != 0) ? S.str = get_next_line_fd_null(fd, S.a, 0), S.a++ : 0;
-  if (S.a != 0 && fd != 0)
-    {
-      G.tmp = S.i;
-      if ((S.i = my_ret_line(S.str, S.i + 1, 0)) == 848484)
-	{
-	  G.buffer = 0;
-	  return (G.buffer);
-	}
-      G.buffer = malloc(sizeof(char) * (S.i - G.tmp));
-      while (G.tmp < S.i)
-	{
-	  G.buffer[G.j] = S.str[G.tmp];
-	  G.tmp++, G.j++;
-	}
-      G.buffer[G.j - 1] = 0;
-    }
-  else
-    G.buffer = get_next_line_fd_null(fd, 1, 0);
-  return (G.buffer);
+  (str[k] == 0) ? dest = 0 : 0;
+  return (dest);
 }
 
 /*int	main(int ac, char **av)
@@ -144,7 +80,7 @@ char	*get_next_line(const int fd)
   if (ac < 2)
     exit (84);
   fd = open(av[1], O_RDONLY);
-  while ((s = get_next_line(fd)) != 0)
+  while (s = get_next_line(fd))
     {
       write(1, s, strlen(s));
       write(1, "\n", 1);
--- get_next_line.h
+++ get_next_line.h
@@ -5,7 +5,7 @@
 ** Login   <miguel.joubert@epitech.eu>
 ** 
 ** Started on  Tue Jan  3 17:01:30 2017 Joubert Miguel
-** Last update Fri Jan  6 17:41:05 2017 Joubert Miguel
+** Last update Mon Jan  9 18:34:02 2017 Joubert Miguel
 */
 
 #ifndef _GET_NEXT_LINE
@@ -19,41 +19,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define READ_SIZE 1
-
-typedef struct s_Getnext
-{
-char	*buffer;
-int   tmp;
-int	j;
-}t_Getnext;
-
-typedef struct s_Static
-{
-char     *str;
-int      i;
-int      a;
-}t_Static;
-
-typedef struct s_StaticFd
-{
-char   buffer[READ_SIZE];
-char   *str;
-}t_StaticFd;
-
-typedef struct s_GetnextFd
-{
-char  *dest;
-int   tmp;
-}t_GetnextFd;
-
-int     my_ret_line(char *str, int i, int a);
+#define READ_SIZE 233
 
 char    *my_strcat(char *dest, char *src);
-
-char    *remalloc(char *str, int size, int a, int i);
-
-char    *get_next_line_fd_null(const int fd, int a, int k);
 
 char    *get_next_line(const int fd);
 
